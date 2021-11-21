1edda9c24a42f64ca43f0fc89db6992db9a0b6e6
zs <1598872182@qq.com>
2021-11-22 01:58:42 +0800

    push test2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("2182");
         System.out.println("887");
         System.out.println("push test");
+        System.out.println("88888888");
     }
 }
 
